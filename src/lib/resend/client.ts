c438b4da6e441046e1fdddfaf4140602a9e3346d
Samuel Harvey <samuelharvey06@gmail.com>
2026-02-16 09:03:13 -0500

    Corrected resend api key env naming

--- src/lib/resend/client.ts
+++ src/lib/resend/client.ts
@@ -1,7 +1,7 @@
 import {Resend} from "resend";
 
 export function createResendClient() {
-    if (!process.env.NEXT_PUBLIC_RESEND_API_KEY) {
+    if (!process.env.RESEND_API_KEY) {
         throw new Error("RESEND_API_KEY is not defined");
     }
 
